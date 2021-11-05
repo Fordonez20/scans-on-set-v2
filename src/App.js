b5cb1ac347dc446e26bebdd9b4b7adcf901ab6dc
Franklin Ordonez <Fordonez20@gmail.com>
2021-11-05 01:37:16 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = axios.create({
+  baseURL: `https://ad37vz2hch.execute-api.us-east-2.amazonaws.com/prod/products`
+})
+
+class App extends Component {
+
+  constructor() {
+    super()
+    api.get('/').then(res => {
+      console.log(res.data)
+    })
+  } 
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Scans On Set
+          </a>
+        </header>
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
