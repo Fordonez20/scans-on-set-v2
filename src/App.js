e38a263e8c14e86e72045fdaf312708f69cf26b5
Franklin Ordonez <Fordonez20@gmail.com>
2021-11-15 19:59:17 -0500

    All 3 - api, db and front end working together. Added a button to update Item Status

--- src/App.js
+++ src/App.js
@@ -1,41 +1,140 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+//import { useEffect } from 'react';
+//import useState from 'react-hook-use-state';
+//import logo from './logo.svg';
+//import styled from 'styled-components';
+
 
 const api = axios.create({
-  baseURL: `https://ad37vz2hch.execute-api.us-east-2.amazonaws.com/prod/products`
+  baseURL: `https://ad37vz2hch.execute-api.us-east-2.amazonaws.com/prod`
 })
 
-class App extends Component {
+class App extends React.Component {
 
-  constructor() {
-    super()
-    api.get('/').then(res => {
-      console.log(res.data)
-    })
-  } 
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Scans On Set
-          </a>
-        </header>
-      </div>
-    );
-
+  state = {
+    items: []
   }
   
+  constructor() {
+    super();
+    this.getItems();
+  } 
+
+  getItems = async () => {
+      let data = await api.get('/products').then(({ data }) => data );
+      console.log(data);
+      this.setState({ items: data.products });
+  }
+
+  createItem = async () => {
+    let res = await api.post('/product', 
+    {
+      "productId": "55555",
+      "productName": "Walkie Numba 55555", 
+      "color": "Some String",
+      "price": 200,
+      "inventory": 10
+
+    })
+    console.log(res);
+    this.getItems();
+  }
+
+  updateItem = async (productId, val) => {
+    if (val === 'OUT') {
+      let data = await api.patch('/product', { 
+        "productId": productId,
+        "updateKey": "color",
+        "updateValue": "IN"
+  
+      })
+
+    } else {
+      let data = await api.patch('/product', { 
+        "productId": productId,
+        "updateKey": "color",
+        "updateValue": "OUT"
+  
+      })
+
+    }
+
+
+    /*let data = await api.patch('/product', { 
+      "productId": productId,
+      "updateKey": "color",
+      "updateValue": val
+
+    })*/
+    this.getItems();
+  }
+
+  render() {
+  return (
+    <div className="App">
+      <header className = "App-header">
+        
+
+        {this.state.items.map(item => 
+        <h2 key={item.productId} /*onClick={() =>this.updateItem(item.productId, `Updated`)}*/>
+          <button onClick={() =>this.updateItem(item.productId, item.color)}> Update This Item</button>
+          {item.productName} {item.color}
+        </h2>)}
+
+        <button /*onClick={this.createItem}*/> Create An Item (off atm)</button>
+
+      </header>
+
+    </div>
+    
+  );
+  }
 }
 
 export default App;
+
+
+/* THIS WORKS ---------------------------------------------------
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://ad37vz2hch.execute-api.us-east-2.amazonaws.com/prod/products')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.products);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+
+
+  return (
+    <ul>
+      {data.map((item) => (
+
+        <li key={item.productId}>{item.productId} {item.productName} {item.color}</li>
+        
+      ))}
+    </ul>
+
+  );
+}
+  
+
+
+
+
+export default App;
+
+*/
